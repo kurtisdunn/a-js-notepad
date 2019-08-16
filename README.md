7224537b2679c7447d1c9ed53381c1691ed23fc7
Kurt <kurt5g@Mac-Pro.local>
2019-08-16 11:11:41 +1000

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # a-js-notepad
 A web based notepad for jotting things on the fly.
+
+## Features
+
+* React 16
+* Webpack 4
+* Babel 7
+
+## Install it
+
+```bash
+git clone git@github.com:kurtisdunn/a-js-notepad.git
+cd a-js-notepad
+npm install
+npm start
+
+```
+
+## Run it
+
+```bash
+npm start
+
+```
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+       test: /\.scss$/,
+       use: [
+         {
+           loader: 'style-loader' // creates style nodes from JS strings
+         },
+         {
+           loader: 'css-loader' // translates CSS into CommonJS
+         },
+         {
+           loader: 'sass-loader' // compiles Sass to CSS
+         }
+       ]
+     },
+     {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  }
+};

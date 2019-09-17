7b88d30628474dea2dd880c5136876ff4a9f065b
Kurt <kurt5g@Mac-Pro.local>
2019-09-17 16:59:47 +1000

    added more stuff.

--- src/components/editor/index.js
+++ src/components/editor/index.js
@@ -16,9 +16,7 @@ export default class Editor extends React.Component {
     console.log('Editor extends React.Component: ', props);
   }
   UNSAFE_componentWillReceiveProps(nextProps) {
-    console.log('UNSAFE_componentWillReceiveProps', nextProps.note[0]);
     this.state.editor.setContents( nextProps.note[0].delta.ops);
-
   }
   componentDidMount() {
     const that = this;
@@ -31,7 +29,7 @@ export default class Editor extends React.Component {
          that.props.callback(that.state.delta);
       }
     });
-
+    that.props.editor(editor);
   }
 
   render() {
